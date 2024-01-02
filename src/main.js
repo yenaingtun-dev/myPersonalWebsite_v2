740a00242b9117fe515527d3e3a016bca45c0b74
Ye Naing Tun <yenaingtun425@gmail.com>
2024-01-02 21:03:47 +0630

    added darkmode

--- src/main.js
+++ src/main.js
@@ -1,47 +1,56 @@
 const lightIcon = document.querySelector(".light-icon");
 const darkIcon = document.querySelector(".dark-icon");
 
-const userTheme = localStorage.getItem("theme");
+let userTheme = "light";
 const systemTheme = window.matchMedia("(prefers-color-scheme: dark)").matches;
 
 // On page load or when changing themes, best to add inline in `head` to avoid FOUC
 const themeCheck = () => {
     if (userTheme === "dark" || (!userTheme && systemTheme)) {
-        console.log("dark");
         document.documentElement.classList.add("dark");
-        darkIcon.parentElement.classList.add("hidden");
-        lightIcon.parentElement.classList.remove("hidden");
+        darkIcon.parentElement.classList.remove("hidden");
+        lightIcon.parentElement.classList.add("hidden");
         return;
     } else {
-        console.log("light");
         document.documentElement.classList.remove("dark");
-        lightIcon.parentElement.classList.add("hidden");
-        darkIcon.parentElement.classList.remove("hidden");
+        darkIcon.parentElement.classList.add("hidden");
+        lightIcon.parentElement.classList.remove("hidden");
         return;
     }
 };
 
-const themeSwitch = () => {
-    if (document.documentElement.classList.contains("dark")) {
-        document.documentElement.classList.remove("dark");
-        localStorage.setItem("theme", "light");
-        darkIcon.parentElement.classList.add("hidden");
-        lightIcon.parentElement.classList.remove("hidden");
-        themeCheck();
-    }
-    document.documentElement.classList.add("dark");
-    localStorage.setItem("theme", "dark");
-    lightIcon.parentElement.classList.add("hidden");
-    darkIcon.parentElement.classList.remove("hidden");
-    themeCheck();
-};
+// const themeSwitch = () => {
+//     if (document.documentElement.classList.contains("dark")) {
+//         document.documentElement.classList.remove("dark");
+//         localStorage.setItem("theme", "light");
+//         darkIcon.parentElement.classList.add("hidden");
+//         lightIcon.parentElement.classList.remove("hidden");
+//         console.log("herer dark to light");
+//         themeCheck();
+//     } else {
+//         document.documentElement.classList.add("dark");
+//         userTheme = "dark";
+//         lightIcon.parentElement.classList.add("hidden");
+//         darkIcon.parentElement.classList.remove("hidden");
+//         console.log("herer light to dark");
+//         themeCheck();
+//     }
+// };
 
 lightIcon.addEventListener("click", () => {
-    themeSwitch();
+    document.documentElement.classList.add("dark");
+    userTheme = "dark";
+    lightIcon.parentElement.classList.add("hidden");
+    darkIcon.parentElement.classList.remove("hidden");
+    // themeSwitch();
 });
 
 darkIcon.addEventListener("click", () => {
-    themeSwitch();
+    document.documentElement.classList.remove("dark");
+    userTheme = "light";
+    lightIcon.parentElement.classList.remove("hidden");
+    darkIcon.parentElement.classList.add("hidden");
+    // themeSwitch();
 });
 
 themeCheck();
